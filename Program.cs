01ceb2f541787e5a23e8c347746d5724e36a9c86
Amelia James <ameliafjjames@gmail.com>
2026-09-19 14:27:15 +0100

    Re-add api endpoint to get opinions

--- Program.cs
+++ Program.cs
@@ -23,6 +23,10 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.MapGet("/api/opinions/random", async (IOpinionService opinionService) =>
+    Results.Ok(await opinionService.GetRandomOpinionAsync()));
+
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
 app.UseHttpsRedirection();
 
--- Services/OpinionService.cs
+++ Services/OpinionService.cs
@@ -9,26 +9,6 @@ public interface IOpinionService
     Task<Opinion?> GetRandomOpinionAsync();
 }
 
-// public class InMemoryOpinionService : IOpinionService
-// {
-//     private readonly Opinion[] opinions =
-//     [
-//         new(OpinionCategory.Happy, "Head scratches approved"),
-//         new(OpinionCategory.Grumpy, "Do not pet the tail"),
-//         new(OpinionCategory.Curious, "He thinks you smell funny"),
-//         new(OpinionCategory.Happy, "Treats are welcome"),
-//         new(OpinionCategory.Grumpy, "Stinky girl cats need to go away")
-//     ];
-
-//     public Opinion GetRandomOpinion()
-//     {
-//         return opinions[Random.Shared.Next(opinions.Length)];
-//     }
-// }
-
-
-
-
 public class OpinionService(GizmoDbContext db) : IOpinionService
 {
     public async Task<Opinion?> GetRandomOpinionAsync()
